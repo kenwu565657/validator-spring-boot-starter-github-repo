91245ab8180e18e8a867db1f3bef7959159c701a
kenwu565657 <kenwu565656@icloud.com>
2025-02-17 20:43:30 +0800

    add example application

---
+++ validator-spring-boot-starter-example/src/main/java/com/validator/example/ValidatorSpringBootStarterExampleApplication.java
@@ -0,0 +1,13 @@
+package com.validator.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorSpringBootStarterExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidatorSpringBootStarterExampleApplication.class, args);
+    }
+
+}
