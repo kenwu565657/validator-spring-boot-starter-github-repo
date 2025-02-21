8042e65908fa0b4e8b93b36bffac215beaf339ec
kenwu565657 <kenwu565656@icloud.com>
2025-02-21 21:03:43 +0800

    remove unused config

--- validator-spring-boot-starter/src/test/java/com/validator/annotation/FieldNameExtractorTestConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.validator.annotation;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "com.validator")
-public class FieldNameExtractorTestConfiguration {
-}
