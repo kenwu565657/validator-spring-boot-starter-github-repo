ef67f2bf1e35cc3f1ed33ed887313cdf678895f0
kenwu565657 <kenwu565656@icloud.com>
2025-02-21 21:05:15 +0800

    update field name extraction logic and unit test

--- validator-spring-boot-starter/src/main/java/com/validator/annotation/FieldNameExtractor.java
+++ validator-spring-boot-starter/src/main/java/com/validator/annotation/FieldNameExtractor.java
@@ -1,10 +1,14 @@
 package com.validator.annotation;
 
-import java.lang.annotation.Annotation;
+import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.AssertTrue;
+
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.AnnotatedParameterizedType;
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -14,48 +18,108 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static java.util.Arrays.stream;
+
 public class FieldNameExtractor {
+
+    private final String fieldSeparator;
+    private final int listLength;
+    private final String listItemPlaceHolder;
+    private final String nodeSeparator = ".";
+
+    public FieldNameExtractor() {
+        this(" - ", 5, "Item Number");
+    }
+
+    public FieldNameExtractor(String fieldSeparator, int listLength, String listItemPlaceHolder) {
+        this.fieldSeparator = fieldSeparator;
+        this.listLength = listLength;
+        this.listItemPlaceHolder = listItemPlaceHolder;
+    }
+
     public <T> Map<String, String> getFieldNames(Class<T> clazz) {
+        String parentFieldName = getClassName(clazz);
         List<String> parentFieldNameList = new ArrayList<>();
+        parentFieldNameList.add(parentFieldName);
         List<String> parentNodeNameList = new ArrayList<>();
         return getFieldNameRecursively(clazz, parentFieldNameList, parentNodeNameList);
     }
 
+    private <T> Map<String, String> getFieldNameRecursively(Class<T> clazz, List<String> parentFieldNameList, List<String> parentNodeNameList) {
+        Field[] fieldArray = clazz.getDeclaredFields();
+        Map<String, String> nodeNameFieldNameRootMap = new HashMap<>();
+        for (Field field : fieldArray) {
+            List<String> parentNodeNameListCopy = new ArrayList<>(parentNodeNameList);
+            List<String> parentFieldNameListCopy = new ArrayList<>(parentFieldNameList);
+            Map<String, String> nodeNameFieldNameMap = getFieldNameRecursively(field, parentFieldNameListCopy, parentNodeNameListCopy);
+            nodeNameFieldNameRootMap.putAll(nodeNameFieldNameMap);
+        }
+        List<Method> methodList = Arrays
+                .stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(AssertFalse.class) || method.isAnnotationPresent(AssertTrue.class))
+                .toList();
+        for (Method method : methodList) {
+            List<String> parentNodeNameListCopy = new ArrayList<>(parentNodeNameList);
+            List<String> parentFieldNameListCopy = new ArrayList<>(parentFieldNameList);
+            String methodFieldName = getMethodName(method);
+            parentFieldNameListCopy.add(methodFieldName);
+            String methodNodeName = getNodeName(method);
+            parentNodeNameListCopy.add(methodNodeName);
+            Map.Entry<String, String> nodeNameFieldNameEntry = Map.entry(
+                    String.join(nodeSeparator, parentNodeNameListCopy),
+                    String.join(fieldSeparator, parentFieldNameListCopy)
+            );
+            nodeNameFieldNameRootMap.put(nodeNameFieldNameEntry.getKey(), nodeNameFieldNameEntry.getValue());
+        }
+        return nodeNameFieldNameRootMap;
+    }
+
     private Map<String, String> getFieldNameRecursively(Field field, List<String> parentFieldNameList, List<String> parentNodeNameList) {
-        if (field.getType().isPrimitive() || field.getType().equals(String.class)) {
-            String leafFieldName = getFieldName(field);
-            parentFieldNameList.add(leafFieldName);
+        if (isPrimitiveOrWrappedOrStringOrEnum(field.getType())) {
             String leafNodeName = getNodeName(field);
             parentNodeNameList.add(leafNodeName);
+            String leafFieldName = getFieldName(field);
+            parentFieldNameList.add(leafFieldName);
+
             Map.Entry<String, String> nodeNameFieldNameEntry = Map.entry(
-                    String.join(".", parentNodeNameList),
-                    String.join(".", parentFieldNameList)
+                    String.join(nodeSeparator, parentNodeNameList),
+                    String.join(fieldSeparator, parentFieldNameList)
             );
             return Map.ofEntries(nodeNameFieldNameEntry);
         }
         else if (Collection.class.isAssignableFrom(field.getType())) {
             String s = field.toGenericString();
             String type = s.split("<")[1].split(">")[0];
-            Class clazz;
+            Class<?> clazz;
             try {
                 clazz = Class.forName(type);
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
-            String leafFieldName = getFieldName(field);
-            parentFieldNameList.add(leafFieldName);
-            String leafNodeName = getNodeName(field);
-            parentNodeNameList.add(leafNodeName);
 
-            AnnotatedType annotatedType = field.getAnnotatedType();
-            AnnotatedParameterizedType annotatedParameterizedType = (AnnotatedParameterizedType) annotatedType;
-            AnnotatedType annotatedActualTypeArgument = annotatedParameterizedType.getAnnotatedActualTypeArguments()[0];
-            String annotatedTypedFieldName = getFieldName(annotatedActualTypeArgument);
-            String annotatedTypedNodeName = getNodeName(annotatedActualTypeArgument);
-            parentFieldNameList.add(annotatedTypedFieldName);
-            parentFieldNameList.add(annotatedTypedNodeName);
+            Map<String, String> listRootFieldNameMap = new HashMap<>();
+            String parentFieldNameListJoinName = String.join(fieldSeparator, parentFieldNameList);
+            String parentNodeNameListJoinName = String.join(nodeSeparator, parentNodeNameList);
+            String listNodeName = parentNodeNameList.isEmpty() ? getNodeName(field) : MessageFormat.format("{0}{1}{2}", parentNodeNameListJoinName, nodeSeparator, getNodeName(field));
+            String listFieldName = parentFieldNameList.isEmpty() ? getFieldName(field) : MessageFormat.format("{0}{1}{2}", parentFieldNameListJoinName, fieldSeparator, getFieldName(field));
+            listRootFieldNameMap.put(listNodeName, listFieldName);
+            for (int i = 0; i < listLength; i++) {
+                List<String> parentNodeNameListCopy = new ArrayList<>(parentNodeNameList);
+                List<String> parentFieldNameListCopy = new ArrayList<>(parentFieldNameList);
+                String turnParentNodeName = MessageFormat.format("{0}[{1}]", listNodeName, i);
+                String turnParentFieldName = MessageFormat.format("{0}{1}{2} {3}", listFieldName, fieldSeparator, listItemPlaceHolder, i + 1);
+                parentNodeNameListCopy.add(turnParentNodeName);
+                parentFieldNameListCopy.add(turnParentFieldName);
+                AnnotatedType annotatedType = field.getAnnotatedType();
+                AnnotatedParameterizedType annotatedParameterizedType = (AnnotatedParameterizedType) annotatedType;
+                AnnotatedType annotatedActualTypeArgument = annotatedParameterizedType.getAnnotatedActualTypeArguments()[0];
+                String annotatedTypedFieldName = getFieldName(annotatedActualTypeArgument);
+                parentFieldNameListCopy.add(annotatedTypedFieldName);
+                Map<String, String> listTurnFieldNameMap = getFieldNameRecursively(clazz, parentFieldNameListCopy, parentNodeNameListCopy);
+                listRootFieldNameMap.putAll(listTurnFieldNameMap);
+            }
 
-            return getFieldNameRecursively(clazz, parentFieldNameList, parentNodeNameList);
+            return listRootFieldNameMap;
         } else {
             String leafFieldName = getFieldName(field);
             parentFieldNameList.add(leafFieldName);
@@ -65,37 +129,43 @@ public class FieldNameExtractor {
         }
     }
 
-    private <T> Map<String, String> getFieldNameRecursively(Class<T> clazz, List<String> parentFieldNameList, List<String> parentNodeNameList) {
-        if (clazz.isPrimitive() || clazz.equals(String.class)) {
-            Map.Entry<String, String> nodeNameFieldNameEntry = Map.entry(
-                    String.join(".", parentNodeNameList),
-                    String.join(".", parentFieldNameList)
-            );
-            return Map.ofEntries(nodeNameFieldNameEntry);
-        }
-
-        Field[] fieldArray = clazz.getFields();
-        Map<String, String> nodeNameFieldNameRootMap = new HashMap<>();
-        for (Field field : fieldArray) {
-            List<String> parentNodeNameListCopy = new ArrayList<>(parentNodeNameList);
-            List<String> parentFieldNameListCopy = new ArrayList<>(parentFieldNameList);
-            Map<String, String> nodeNameFieldNameMap = getFieldNameRecursively(field, parentFieldNameListCopy, parentNodeNameListCopy);
-            nodeNameFieldNameRootMap.putAll(nodeNameFieldNameMap);
-        }
-        return nodeNameFieldNameRootMap;
-    }
-
     private String getFieldName(Field field) {
         if (field.isAnnotationPresent(FieldName.class)) {
             return getFieldNameForFieldNameAnnotatedElement(field);
         }
+        if (field.getType().isAnnotationPresent(FieldName.class)) {
+            return getFieldNameForFieldNameAnnotatedElement(field.getType());
+        }
         return getNodeName(field);
+    }
+
+    private <T> String getClassName(Class<T> clazz) {
+        if (clazz.isAnnotationPresent(FieldName.class)) {
+            return getFieldNameForFieldNameAnnotatedElement(clazz);
+        }
+        return getNodeName(clazz);
+    }
+
+    private <T> String getNodeName(Class<T> clazz) {
+        return clazz.getSimpleName();
+    }
+
+    private String getMethodName(Method method) {
+        if (method.isAnnotationPresent(FieldName.class)) {
+            return getFieldNameForFieldNameAnnotatedElement(method);
+        }
+        return getNodeName(method);
+    }
+
+    private String getNodeName(Method method) {
+        String methodName = method.getName();
+        return Character.toLowerCase(methodName.charAt(2)) + methodName.substring(3);
     }
 
     private String getFieldNameForFieldNameAnnotatedElement(AnnotatedElement annotatedElement) {
         FieldName[] fieldNameAnnotationArray = annotatedElement.getAnnotationsByType(FieldName.class);
-        List<String> fieldNameValueList = Arrays.stream(fieldNameAnnotationArray).map(FieldName::value).toList();
-        return String.join(".", fieldNameValueList);
+        List<String> fieldNameValueList = stream(fieldNameAnnotationArray).map(FieldName::value).toList();
+        return String.join(nodeSeparator, fieldNameValueList);
     }
 
     private String getNodeName(Field field) {
@@ -106,10 +176,35 @@ public class FieldNameExtractor {
         if (annotatedType.isAnnotationPresent(FieldName.class)) {
             return getFieldNameForFieldNameAnnotatedElement(annotatedType);
         }
-        return getNodeName(annotatedType);
+        if (annotatedType.getClass().isAnnotationPresent(FieldName.class)) {
+            return getFieldNameForFieldNameAnnotatedElement(annotatedType.getClass());
+        }
+        return "Item";
     }
 
-    private String getNodeName(AnnotatedType annotatedType) {
-        return "item";
+    private boolean isPrimitiveOrWrappedOrStringOrEnum(Class<?> clazz) {
+        return clazz.isPrimitive() || isWrapperType(clazz) || String.class.equals(clazz) || clazz.isEnum();
+    }
+
+    private final Set<Class<?>> WRAPPER_TYPES = getWrapperTypes();
+
+    private boolean isWrapperType(Class<?> clazz)
+    {
+        return WRAPPER_TYPES.contains(clazz);
+    }
+
+    private Set<Class<?>> getWrapperTypes()
+    {
+        Set<Class<?>> ret = new HashSet<>();
+        ret.add(Boolean.class);
+        ret.add(Character.class);
+        ret.add(Byte.class);
+        ret.add(Short.class);
+        ret.add(Integer.class);
+        ret.add(Long.class);
+        ret.add(Float.class);
+        ret.add(Double.class);
+        ret.add(Void.class);
+        return ret;
     }
 }
--- validator-spring-boot-starter/src/test/java/com/validator/annotation/FieldNameExtractorTest.java
+++ validator-spring-boot-starter/src/test/java/com/validator/annotation/FieldNameExtractorTest.java
@@ -1,16 +1,15 @@
 package com.validator.annotation;
 
+import com.validator.test.TestingOnlineShoppingForm;
+import com.validator.test.TestingOnlineShoppingFormNotAnnotated;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FieldNameExtractorTest {
@@ -20,38 +19,234 @@ class FieldNameExtractorTest {
     @Nested
     public class testGetFieldNames {
         @Test
-        void getFieldNames() {
+        void getFieldNameMap() {
             var fieldNameExtractor = new FieldNameExtractor();
-            Map<String, String> fieldNameSet = fieldNameExtractor.getFieldNames(TestingObject.class);
-            logger.info(fieldNameSet.toString());
+            Map<String, String> fieldNameMap = fieldNameExtractor.getFieldNames(TestingOnlineShoppingForm.class);
+            logger.info("Field Name Map: {}", fieldNameMap);
+            assertContainEntry(fieldNameMap, Map.entry("id", "Online Shopping Form - Shopping Form Id"));
+            assertContainEntry(fieldNameMap, Map.entry("firstname", "Online Shopping Form - Contact First Name"));
+            assertContainEntry(fieldNameMap, Map.entry("lastname", "Online Shopping Form - Contact Last Name"));
+            assertContainEntry(fieldNameMap, Map.entry("emailAddress", "Online Shopping Form - Contact Email Address"));
+            assertContainEntry(fieldNameMap, Map.entry("phoneNumber", "Online Shopping Form - Contact Phone Number"));
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "shoppingItemList[0].itemId",
+                            "Online Shopping Form - Online Shopping Form - Shopping Cart - Item Number 1 - Shopping Cart Item - Item Name"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "shoppingItemList[0].quantity",
+                            "Online Shopping Form - Online Shopping Form - Shopping Cart - Item Number 1 - Shopping Cart Item - Item Quantity"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "shippingMethod",
+                            "Online Shopping Form - Selected Shipping Method"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.collectionPointId",
+                            "Online Shopping Form - Self Collection Form - Self Collection Point Pin Number"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.phoneNumber",
+                            "Online Shopping Form - Self Collection Form - Contact Phone Number"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.emailAddress",
+                            "Online Shopping Form - Self Collection Form - Contact Email Address"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.eitherPhoneNumberOrEmailAddressProvided",
+                            "Online Shopping Form - Self Collection Form - Phone Number Or Email Address"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.line1",
+                            "Online Shopping Form - Address Information - Address Line 1"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.line2",
+                            "Online Shopping Form - Address Information - Address Line 2"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.line3",
+                            "Online Shopping Form - Address Information - Address Line 3"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.district",
+                            "Online Shopping Form - Address Information - Address District"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.eitherAddressLineProvided",
+                            "Online Shopping Form - Address Information - Address Line 1 Or 2 Or 3"
+                    )
+            );
+        }
+
+        @Test
+        void getFieldNameMapFromNotAnnotatedClass() {
+            var fieldNameExtractor = new FieldNameExtractor();
+            Map<String, String> fieldNameMap = fieldNameExtractor.getFieldNames(TestingOnlineShoppingFormNotAnnotated.class);
+            logger.info("Field Name Map: {}", fieldNameMap);
+            assertContainEntry(fieldNameMap, Map.entry("id", "TestingOnlineShoppingFormNotAnnotated - id"));
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "firstname",
+                            "TestingOnlineShoppingFormNotAnnotated - firstname"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "lastname",
+                            "TestingOnlineShoppingFormNotAnnotated - lastname"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "emailAddress",
+                            "TestingOnlineShoppingFormNotAnnotated - emailAddress"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "phoneNumber",
+                            "TestingOnlineShoppingFormNotAnnotated - phoneNumber"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "shoppingItemList",
+                            "TestingOnlineShoppingFormNotAnnotated - shoppingItemList"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "shoppingItemList[0].itemId",
+                            "TestingOnlineShoppingFormNotAnnotated - TestingOnlineShoppingFormNotAnnotated - shoppingItemList - Item Number 1 - Item - itemId"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "shoppingItemList[0].quantity",
+                            "TestingOnlineShoppingFormNotAnnotated - TestingOnlineShoppingFormNotAnnotated - shoppingItemList - Item Number 1 - Item - quantity"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "shippingMethod",
+                            "TestingOnlineShoppingFormNotAnnotated - shippingMethod"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.collectionPointId",
+                            "TestingOnlineShoppingFormNotAnnotated - Self Collection Form - Self Collection Point Pin Number"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.phoneNumber",
+                            "TestingOnlineShoppingFormNotAnnotated - Self Collection Form - Contact Phone Number"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.emailAddress",
+                            "TestingOnlineShoppingFormNotAnnotated - Self Collection Form - Contact Email Address"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "selfCollectionForm.eitherPhoneNumberOrEmailAddressProvided",
+                            "TestingOnlineShoppingFormNotAnnotated - Self Collection Form - Phone Number Or Email Address"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.line1",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm - line1"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.line2",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm - line2"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.line3",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm - line3"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.district",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm - district"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm.eitherAddressLineProvided",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm - eitherAddressLineProvided"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm2.line1",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm2 - line1"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm2.line2",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm2 - line2"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm2.line3",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm2 - line3"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm2.district",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm2 - district"
+                    )
+            );
+            assertContainEntry(fieldNameMap,
+                    Map.entry(
+                            "addressForm2.eitherAddressLineProvided",
+                            "TestingOnlineShoppingFormNotAnnotated - addressForm2 - eitherAddressLineProvided"
+                    )
+            );
         }
     }
 
-    @FieldNameRoot
-    public static class TestingObject {
-        @FieldName("name1")
-        public String name;
-
-        public String name2;
-
-        @FieldName("testingObject2")
-        public TestingObject2 testingObject2;
-
-        @FieldName("nameList2")
-        public List<@FieldName("name4") String> nameList;
-
-        public static class Field {
-            public static String name = "name";
-            public static String name2 = "name2";
-        }
+    private <K, V> void assertContainEntry(Map<K, V> map, Map.Entry<K, V> entry) {
+        Assertions.assertTrue(map.containsKey(entry.getKey()));
+        V value = map.get(entry.getKey());
+        Assertions.assertNotNull(value);
+        Assertions.assertEquals(entry.getValue(), value);
     }
-
-    public static class TestingObject2 {
-        @FieldName("name3")
-        public String name;
-
-        @FieldName("nameList")
-        public List<@FieldName("name4") String> nameList;
-    }
-
 }
