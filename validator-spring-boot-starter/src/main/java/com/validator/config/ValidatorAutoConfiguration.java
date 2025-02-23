bb93112500586f5f920f15bb8b4a1ae19656e3e9
kenwu565657 <kenwu565656@icloud.com>
2025-02-23 16:38:05 +0800

    update auto config

--- validator-spring-boot-starter/src/main/java/com/validator/config/ValidatorAutoConfiguration.java
+++ validator-spring-boot-starter/src/main/java/com/validator/config/ValidatorAutoConfiguration.java
@@ -1,24 +1,14 @@
 package com.validator.config;
 
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import com.validator.service.ValidationService;
+import com.validator.service.ValidationServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ValidatorAutoConfiguration {
-
     @Bean
-    @ConditionalOnMissingBean
-    Validator validator(ValidatorFactory validatorFactory) {
-        return validatorFactory.getValidator();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    ValidatorFactory validatorFactory() {
-        return Validation.buildDefaultValidatorFactory();
+    ValidationService validationService() {
+        return new ValidationServiceImpl();
     }
 }
