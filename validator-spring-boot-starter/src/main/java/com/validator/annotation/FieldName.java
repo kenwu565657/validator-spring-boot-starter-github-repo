316a913448e107537a47fbd626b88379b4efd4f9
kenwu565657 <kenwu565656@icloud.com>
2025-02-23 16:36:17 +0800

    remove unused code

--- validator-spring-boot-starter/src/main/java/com/validator/annotation/FieldName.java
+++ validator-spring-boot-starter/src/main/java/com/validator/annotation/FieldName.java
@@ -1,5 +1,7 @@
 package com.validator.annotation;
 
+import com.validator.constant.LocaleConstant;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
